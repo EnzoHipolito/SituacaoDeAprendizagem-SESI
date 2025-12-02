78fe89d2dcf9ee9b546a3ab25e43c79d29cb5704
unknown <enzo_hipolito@estudante.sesisenai.org.br>
2025-12-02 00:22:43 -0300

    Concluido toda a pesquisa (Falta 2 scripts)

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const resP = document.getElementById('resP');
 const resLD = document.getElementById('resLD');
 const menuBtn = document.getElementById('menuBtn');
 const sideBar = document.getElementById('sideBar');
+const btnDados = document.getElementById('btnDados');
+const resDados = document.getElementById('resDados');
 
 menuBtn.addEventListener('click', () => {
     sideBar.classList.toggle('active');
+})
+
+btnDados.addEventListener('click', () =>{
+    let nome = document.getElementById('nome').value
+    let idade = Number(document.getElementById('idade').value)
+
+    resDados.innerHTML = ''
+    resDados.innerHTML += `Seu nome é ${nome}<br>`
+    resDados.innerHTML += `Você tem ${idade} Anos<br>`
 })
 
 select.addEventListener('change', () => {
@@ -27,6 +38,10 @@ select.addEventListener('change', () => {
         history.pushState({}, "", window.location.pathname);
     }
 });
+
+function gerarAleatorio(min,max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
 function principalOHM(){
     let corrente = Number(document.getElementById('corrente').value)    
@@ -80,5 +95,91 @@ function principalLD(){
     }
     if(sensor > 716.1 ){
         resLD.innerHTML += `Alta intensidade`
+    }
+}
+
+function ordenarNumeros() {
+    let resOrdenar = document.getElementById("resOrdenar");
+    let arr = [];
+    for (let i = 1; i <= 20; i++) {
+    arr.push(i);
+    }
+    resOrdenar.innerHTML = "Crescente: " + arr + "<br>";
+    resOrdenar.innerHTML += "Decrescente: " + arr.slice().reverse();
+}
+
+function BubbleSort() {
+    let resBubble = document.getElementById("resBubble");
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr[i] = gerarAleatorio(1,20)
+    }
+
+    for (let j = 0; j < arr.length - 1; j++) {
+        for (let i = 0; i < arr.length - j - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let aux = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = aux;
+            }
+        }
+    }
+
+    resBubble.innerHTML = "Array ordenado: " + arr;
+}
+
+function somarAleatorios() {
+    let resSoma = document.getElementById("resSoma");
+    let arr = [];
+    let soma = 0
+
+    resSoma.innerHTML = ''
+
+    for (let i = 0; i < 10; i++) {
+        arr[i] = gerarAleatorio(1,220)
+        soma += arr[i]
+    }
+
+    resSoma.innerHTML += `Numeros: ${arr} <br>`
+    resSoma.innerHTML += `Soma: ${soma}`
+}
+
+function calcularIMC() {
+    let resIMC = document.getElementById("resIMC");
+    let altura = Number(document.getElementById("altura").value);
+    let peso = Number(document.getElementById("peso").value);
+    let imc = 0
+
+    resIMC.innerHTML = ''
+
+    if (altura <= 0) {
+        res.innerHTML = "Altura inválida!";
+    }else if (peso <= 0) {
+        res.innerHTML = "Peso inválido!";
+    }else{
+        imc = peso / (altura * altura)
+        resIMC.innerHTML += `IMC: ${imc.toFixed(2)}`
+    }
+}
+
+function converterTemperatura() {
+    let resTemp = document.getElementById("resTemp");
+    let conversao = document.getElementById("conversao").value.toUpperCase();
+    let temperatura = Number(document.getElementById("temperatura").value);
+    
+    res.innerHTML = "";
+
+    if (isNaN(temperatura) || temperatura <= 0) {
+        res.innerHTML = "Temperatura inválida!";
+    }else{
+        if (conversao === "C") {
+            let resultado = (temperatura - 32) * (5 / 9);
+            res.innerHTML += `${temperatura} °F = ${resultado.toFixed(2)} °C"`;
+        } else if (conversao === "F") {
+            let resultado = (temperatura * 9 / 5) + 32;
+            res.innerHTML += `${temperatura} °F = ${resultado.toFixed(2)} °C"`;
+        } else {
+            res.innerHTML = "Digite 'C' ou 'F'.";
+        }
     }
 }
